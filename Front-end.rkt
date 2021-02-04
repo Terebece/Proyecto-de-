b55b656f4997ed462dfdb24f761230be1f316a5a
Miguel <miguel.atorress@ciencias.unam.mx>
2021-02-03 19:01:34 -0600

    Cambios en predicado para tipos

--- Front-end.rkt
+++ Front-end.rkt
@@ -3,7 +3,7 @@
 #|
 Compiladores 2021-1
 Profesora: Dra. Lourdes del Carmen Gonzalez Huesca
-Ayudante: Juan Alfonso Garduño Solís
+Ayudante: Juan Alfonso Garduño Solis
 Laboratorio: Fernando Abigail Galicia Mendoza
 EQUIPO: VeryBlueBerries
 **** Teresa Becerril Torres 315045132
@@ -47,7 +47,7 @@ EQUIPO: VeryBlueBerries
 (define (b-type? x) (memq x '(Bool Char Int List Lambda)))
 
 ;; For Lambda type
-(define (c-type? x) (if (list? x)
+(define (c-type? x) (if (and (list? x) (equal? (length x) 3))
         (let* (
                 [f (car x)]
                 [s (cadr x)]
